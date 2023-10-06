802747f85c5059ca5d2212cc7b00ec4a991f54d0
Leonardo <oliveirabalsa2@gmail.com>
2023-10-06 01:36:26 -0300

    feat(router): add router and config paths

--- cmd/main.go
+++ cmd/main.go
@@ -6,45 +6,33 @@ import (
 
 	"github.com/gin-gonic/gin"
 	"github.com/joho/godotenv"
-	"github.com/oliveirabalsa/balsacar-be/internal/entity"
+	"github.com/oliveirabalsa/balsacar-be/internal/config"
 	"github.com/oliveirabalsa/balsacar-be/internal/handler"
 	"github.com/oliveirabalsa/balsacar-be/internal/repository"
 	"github.com/oliveirabalsa/balsacar-be/internal/service"
-	"gorm.io/driver/postgres"
-	"gorm.io/gorm"
+	"github.com/oliveirabalsa/balsacar-be/router"
 )
 
 func main() {
-	router := gin.Default()
+	server := gin.Default()
 	err := godotenv.Load()
 
-	dbHost := os.Getenv("DB_HOST")
-	dbPort := os.Getenv("DB_PORT")
-	dbUser := os.Getenv("DB_USER")
-	dbPassword := os.Getenv("DB_PASSWORD")
-	dbName := os.Getenv("DB_NAME")
-
-	dsn := fmt.Sprintf("host=%s port=%s user=%s password=%s dbname=%s sslmode=disable", dbHost, dbPort, dbUser, dbPassword, dbName)
-	db, err := gorm.Open(postgres.Open(dsn), &gorm.Config{})
 	if err != nil {
-		panic("Failed to connect to the database")
+		panic(err)
 	}
 
-	db.Table("advertisements").AutoMigrate(&entity.Advertisement{})
+	db := config.InitDB()
+	port := os.Getenv("PORT")
+
+	if port == "" {
+		port = "8080"
+	}
 
 	advertisementRepository := repository.NewAdvertisementRepository(db)
 	advertisementService := service.NewAdvertisementService(advertisementRepository)
 	advertisementHandler := handler.NewAdvertisementHandler(advertisementService)
 
-	api := router.Group("/api")
-	{
-		api.POST("/advertisements", advertisementHandler.CreateAdvertisementHandler)
-		api.GET("/advertisements/:id", advertisementHandler.GetAdvertisementByIDHandler)
-		api.GET("/advertisements", advertisementHandler.GetAllAdvertisementsHandler)
-		api.PUT("/advertisements/:id", advertisementHandler.UpdateAdvertisementHandler)
-		api.DELETE("/advertisements/:id", advertisementHandler.DeleteAdvertisementHandler)
-		api.POST("/advertisements/upload", advertisementHandler.UploadSheetAdvertisementHandler)
-	}
+	router.InitRouter(server, advertisementHandler)
 
-	router.Run(":8080")
+	server.Run(fmt.Sprintf(":%s", port))
 }
---
+++ internal/config/db.go
@@ -0,0 +1,29 @@
+package config
+
+import (
+	"fmt"
+	"os"
+
+	"github.com/oliveirabalsa/balsacar-be/internal/entity"
+	"gorm.io/driver/postgres"
+	"gorm.io/gorm"
+)
+
+func InitDB() *gorm.DB {
+
+	dbHost := os.Getenv("DB_HOST")
+	dbPort := os.Getenv("DB_PORT")
+	dbUser := os.Getenv("DB_USER")
+	dbPassword := os.Getenv("DB_PASSWORD")
+	dbName := os.Getenv("DB_NAME")
+
+	dsn := fmt.Sprintf("host=%s port=%s user=%s password=%s dbname=%s sslmode=disable", dbHost, dbPort, dbUser, dbPassword, dbName)
+	db, err := gorm.Open(postgres.Open(dsn), &gorm.Config{})
+	if err != nil {
+		panic("Failed to connect to the database")
+	}
+
+	db.Table("advertisements").AutoMigrate(&entity.Advertisement{})
+
+	return db
+}
